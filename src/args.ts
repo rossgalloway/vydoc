230dbb06aa23bbae55106af78cc52d06771eab05
Ross <therossgalloway@gmail.com>
2024-10-08 18:34:33 -0400

    logo update

--- src/args.ts
+++ src/args.ts
@@ -1,43 +1,44 @@
-import { cliArgumentsDefault } from "./types";
-import { pipe, concat, toPairs, pick } from "ramda";
-import { option } from "yargs";
-import { blue } from "chalk";
-import { makeTable } from "./table";
+import { cliArgumentsDefault } from './types'
+import { pipe, concat, toPairs, pick } from 'ramda'
+import { option } from 'yargs'
+import { blue } from 'chalk'
+import { makeTable } from './table'
 
-export const options = option("input", {
-  alias: "i",
-  type: "string",
-  description: "contracts dir",
+export const options = option('input', {
+  alias: 'i',
+  type: 'string',
+  description: 'contracts dir',
   default: cliArgumentsDefault.input,
 })
-  .option("output", {
-    alias: "o",
-    type: "string",
-    description: "docs output dir",
+  .option('output', {
+    alias: 'o',
+    type: 'string',
+    description: 'docs output dir',
     default: cliArgumentsDefault.output,
   })
-  .option("compiler", {
-    alias: "c",
-    type: "string",
-    description: "Vyper compiler path",
+  .option('compiler', {
+    alias: 'c',
+    type: 'string',
+    description: 'Vyper compiler path',
     default: cliArgumentsDefault.compiler,
   })
-  .option("format", {
-    alias: "f",
-    type: "string",
-    description: "ddocs format",
+  .option('format', {
+    alias: 'f',
+    type: 'string',
+    description: 'ddocs format',
     default: cliArgumentsDefault.format,
   })
-  .option("template", {
-    alias: "t",
-    type: "string",
-    description: "template to use",
-    default: cliArgumentsDefault.template, 
+  .option('template', {
+    alias: 't',
+    type: 'string',
+    description: 'template to use',
+    default: cliArgumentsDefault.template,
   })
   .help()
-  .alias("help", "h");
+  .alias('help', 'h')
 
-const logo = `
+/*
+  const logo = `
 ███████████████████████████
 █─█─█──█──█────██────█────█
 █─█─██───██─██──█─██─█─██─█
@@ -45,13 +46,24 @@ const logo = `
 █───███─███─██──█─██─█─██─█
 ██─████─███────██────█────█
 ███████─███████████████████
-`;
+`
+*/
+
+const logo = `
+███████████████████████████
+█─█─█──█──█────██────█────█
+█─█─██───██─██──█─██─█─██─█
+      edited by Yearn
+█───███─███─██──█─██─█─██─█
+██─████─███────██────█────█
+███████─███████████████████
+`
 
 export const printCliArguments = pipe(
   pick(Object.keys(cliArgumentsDefault)),
   toPairs,
-  makeTable({ head: ["VARIABLE", "VALUE"], colWidths: [15, 75] }),
+  makeTable({ head: ['VARIABLE', 'VALUE'], colWidths: [15, 75] }),
   blue,
   concat(`${blue(logo)}\n\n`),
   console.log
-);
+)
