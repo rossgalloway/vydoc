e04615d0a3d1369060c92dd6e8cc60d9265b33b4
Ross <therossgalloway@gmail.com>
2024-10-09 15:08:43 -0400

    add .sol files

--- src/bin/vydoc.ts
+++ src/bin/vydoc.ts
@@ -1,22 +1,24 @@
 #!/usr/bin/env node
 
-import { pipe, tap } from "ramda";
-import { CliArguments } from "../types";
-import { options, printCliArguments } from "../args";
-import { getRecursiveFilesByExtensions } from "../utils";
-import { blue } from "chalk";
-import { compileContracts } from "../compile";
-import { generateDocs } from "../generate";
+import { pipe, tap } from 'ramda'
+import { CliArguments } from '../types'
+import { options, printCliArguments } from '../args'
+import { getRecursiveFilesByExtensions } from '../utils'
+import { blue } from 'chalk'
+import { compileContracts } from '../compile'
+import { generateDocs } from '../generate'
 
-const vyperFileExtentions = ["vy"];
+const vyperFileExtentions = ['vy', 'sol']
 
-(async (argv: CliArguments) =>
+;(async (argv: CliArguments) =>
   pipe(
     tap(printCliArguments),
     () => getRecursiveFilesByExtensions(argv.input, vyperFileExtentions),
-    tap(() => console.log(blue("\nCompiling contracts..."))),
+    tap(() => console.log(blue('\nCompiling contracts...'))),
     async (filePaths) =>
       await compileContracts(argv.input, filePaths, argv.compiler)
-        .then(tap(() => console.log(blue("\nGenerate docs..."))))
-        .then((contracts) => generateDocs(argv.format, argv.output, contracts, argv.template))
-  )(argv))(options.argv);
+        .then(tap(() => console.log(blue('\nGenerate docs...'))))
+        .then((contracts) =>
+          generateDocs(argv.format, argv.output, contracts, argv.template)
+        )
+  )(argv))(options.argv)
