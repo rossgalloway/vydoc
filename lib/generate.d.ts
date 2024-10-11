48768fe621bcf3dcdf6274c15d462abbea26473c
Ross <therossgalloway@gmail.com>
2024-10-11 17:15:42 -0400

    build

--- lib/generate.d.ts
+++ lib/generate.d.ts
@@ -1,4 +1,4 @@
-import { Contract, Format } from "./types";
+import { Contract, Format } from './types';
 export declare const generateDocs: (format: Format, output: string, contracts: {
     fileName: string;
     data: Contract;
